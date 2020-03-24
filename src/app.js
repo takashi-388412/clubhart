59c85ef3d4da8fdc994947817d0869ae7fbc792e
takashi388412 <take388412@gmail.com>
2020-03-24 21:07:14 +0900

    add top to image-slider

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
-//JQERUy
+// Top img-slider (swiper.js)
+$(function () {
+  var mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    loop: true, // ループの指定
+    effect: "fade", //フェードの指定
+    autoplay: {
+    delay: 4000, //４秒後に次のスライドへ
+    disableOnInteraction: false //ユーザー側で操作してもスライドを止めない
+    },
+    speed: 2000, //２秒かけてフェードで切り替わる
+  });
+});
