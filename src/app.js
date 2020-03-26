8c3ae1360064e96be00028d8f2c4c21f09eb08d1
takashi388412 <take388412@gmail.com>
2020-03-26 16:48:38 +0900

    update

--- src/app.js
+++ src/app.js
@@ -37,7 +37,18 @@ $(function () {
     sp_menu_target.removeClass('active');
   });
 });
-//smooth scroll
+//scroll-top
+$(function () {
+  var Backtotop = $('.scroll-top');
+  //スルスルっとスクロールでトップへもどる
+  Backtotop.click(function () {
+    $('body,html').animate({
+      scrollTop: 0
+    }, 2500);
+    return false;
+  });
+});
+//scroll-down
 $(function(){
   $('a[href^="#"]').click(function(){
     var speed = 500;
@@ -46,5 +57,20 @@ $(function(){
     var position = target.offset().top;
     $("html, body").animate({scrollTop:position}, speed, "swing");
     return false;
+  });
+});
+//fade-in
+$(function() {
+　$(window).scroll(function (){
+    $('.fade-up').each(function(){
+        var elemPos = $(this).offset().top;
+        var scroll = $(window).scrollTop();
+        var windowHeight = $(window).height();
+      if (scroll > elemPos - windowHeight + windowHeight/2){
+          $(this).addClass('effect-scroll');
+      } else {
+        $(this).removeClass('effect-scroll');
+      }
+    });
   });
 });
