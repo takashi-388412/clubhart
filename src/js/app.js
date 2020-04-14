0fd26cd0d24bdc050707e3441fdd67d07662c67d
takashi388412 <take388412@gmail.com>
2020-04-14 20:53:15 +0900

    update

--- src/js/app.js
+++ src/js/app.js
@@ -1,26 +1,3 @@
-// Top img-slider (swiper.js)
-$(function () {
-  var mySwiper = new Swiper('.swiper-container', {
-    // Optional parameters
-    loop: true, // ループの指定
-    effect: "fade", //フェードの指定
-    autoplay: {
-    delay: 4000, //４秒後に次のスライドへ
-    disableOnInteraction: false //ユーザー側で操作してもスライドを止めない
-    },
-    speed: 2000, //２秒かけてフェードで切り替わる
-  });
-});
-// sticky header
-// $(function () {
-//   jQuery(window).on('scroll', function ($) {
-//     if (550 < jQuery(this).scrollTop()) {
-//       jQuery('#header').addClass('m_scroll');
-//     } else {
-//       jQuery('#header').removeClass('m_scroll');
-//     }
-//   });
-// });
 //hamberger-menu click(open close)
 $(function () {
   var sp_menu = $('.hamberger-menu');
